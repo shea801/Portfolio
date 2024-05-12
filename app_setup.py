b57f6ee7e7dba921e5aa972b8ace1347cfcc6a10
shea801 <111644799+shea801@users.noreply.github.com>
2024-05-12 14:56:04 -0400

    Update app_setup.py

--- app_setup.py
+++ app_setup.py
@@ -2,11 +2,11 @@ import os, pkg_resources, subprocess, sys
 
 # install the necessary modules and libraries to run python application
 # create set of needed libraries and modules for the app
-apps = {'matplotlib',
-        'plotly',
-        'dash',
+apps = {'dash',
         'numpy',
         'pandas',
+        'plotly',
+        'dash_bootstrap_components'
        }
 
 # use pkg_resources module to verify modules that are already present
